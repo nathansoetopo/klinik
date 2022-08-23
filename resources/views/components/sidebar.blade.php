f3334b33113552fb7aa5e60765f75f3d4b6fc266
nathansoetopo <nathan_aris2121@student.uns.ac.id>
2022-08-23 21:43:46 +0700

    Nav, Sidebar, Dashboard on progress fe

---
+++ resources/views/components/sidebar.blade.php
@@ -0,0 +1,21 @@
+<div class="main-sidebar">
+    <aside id="sidebar-wrapper">
+        <div class="sidebar-brand">
+            <a href="index.html">Stisla</a>
+        </div>
+        <div class="sidebar-brand sidebar-brand-sm">
+            <a href="index.html">St</a>
+        </div>
+        <ul class="sidebar-menu">
+            <li><a class="nav-link active" href="credits.html"><i class="fas fa-columns"></i> <span>Dashboard</span></a>
+            <li><a class="nav-link" href="credits.html"><i class="fas fa-user"></i> <span>Data Pasien</span></a>
+            </li>
+        </ul>
+        <div class="hide-sidebar-mini" style="position:fixed; bottom:3vh; width:250px;">
+            <hr style="border-radius: 5px; border: 2px solid #66CDAA; margin-left:5%; margin-right:5%">
+            <ul class="sidebar-menu">
+                <li><a class="nav-link" href="credits.html"><i class="fas fa-power-off"></i> <span>Log Out</span></a>
+            </ul>
+        </div>
+    </aside>
+</div>
