f45f7c35c009e3646a83e5e1c1864cc9d7b2928c
nathansoetopo <nathan_aris2121@student.uns.ac.id>
2022-08-25 00:37:31 +0700

    Load Asset

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -13,8 +13,8 @@
         integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
 
     <!-- Template CSS -->
-    <link rel="stylesheet" href="{{ asset('stisla/assets/css/style.css') }}">
-    <link rel="stylesheet" href="{{asset('stisla/assets/css/components.css')}}">
+    <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}">
+    <link rel="stylesheet" href="{{asset('assets/css/components.css')}}">
 </head>
 
 <body style="background-image: url('login.png'); background-repeat:no-repeat; background-size: cover;">
@@ -86,13 +86,13 @@
     </script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.nicescroll/3.7.6/jquery.nicescroll.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.24.0/moment.min.js"></script>
-    <script src="{{asset('stisla/assets/js/stisla.js')}}"></script>
+    <script src="{{asset('assets/js/stisla.js')}}"></script>
 
     <!-- JS Libraies -->
 
     <!-- Template JS File -->
-    <script src="{{asset('stisla/assets/js/scripts.js')}}"></script>
-    <script src="{{asset('stisla/assets/js/custom.js')}}"></script>
+    <script src="{{asset('assets/js/scripts.js')}}"></script>
+    <script src="{{asset('assets/js/custom.js')}}"></script>
 
     <!-- Page Specific JS File -->
 </body>
--- resources/views/parent/master.blade.php
+++ resources/views/parent/master.blade.php
@@ -13,8 +13,8 @@
         integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
 
     <!-- Template CSS -->
-    <link rel="stylesheet" href="{{ asset('stisla/assets/css/style.css') }}">
-    <link rel="stylesheet" href="{{asset('stisla/assets/css/components.css')}}">
+    <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}">
+    <link rel="stylesheet" href="{{asset('assets/css/components.css')}}">
     <style>
         .active {
             background-color: whitesmoke;
@@ -55,11 +55,11 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.24.0/moment.min.js"></script>
     <script src="{{asset('stisla/assets/js/stisla.js')}}"></script>
     <!-- Template JS File -->
-    <script src="{{asset('stisla/assets/js/scripts.js')}}"></script>
-    <script src="{{asset('stisla/assets/js/custom.js')}}"></script>
+    <script src="{{asset('assets/js/scripts.js')}}"></script>
+    <script src="{{asset('assets/js/custom.js')}}"></script>
 
     <!-- Page Specific JS File -->
-    <script src="{{asset('stisla/assets/js/page/index.js')}}"></script>
+    <script src="{{asset('assets/js/page/index.js')}}"></script>
     {{-- Chart Js --}}
     <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.4/Chart.js"></script>
     {{-- Pie --}}
